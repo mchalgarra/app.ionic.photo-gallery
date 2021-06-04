903898d7036f873a039ba958e963ae8cf3a9cdba
Michell Algarra <michell.algarra@gmail.com>
2021-06-04 18:19:54 -0300

    feat(tab2/*): implemented a simple photo gallery

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  constructor(public readonly photoService: PhotoService) {}
 
+  async ngOnInit(): Promise<void> {
+    await this.photoService.loadSavedPhotos();
+  }
+
+  public addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
